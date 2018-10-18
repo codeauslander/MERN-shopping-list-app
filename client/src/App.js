fa00eeaaecffcf92caaa4764a7de28be873ec3e3
codeauslander <cesar.gusto.catano@gmail.com>
2018-10-18 00:12:31 -0500

    Added redux react-redux redux-thunk, Added store reducers/index.js actions/types actions/itemActions, Making getItems from reducer with initial state

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,19 @@ import './App.css';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar />
-        <ShoppingList />
-        <h1>Hello</h1>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <ShoppingList />
+          <h1>Hello</h1>
+        </div>
+      </Provider> 
     );
   }
 }
