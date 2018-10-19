87cefe7c35bb4b2cceeed67efc1acb9730554d6d
codeauslander <cesar.gusto.catano@gmail.com>
2018-10-19 18:40:00 -0500

    Setting up to push to heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require("express");
+ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require('path');
 
 const items = require('./routes/api/items.js');
 
@@ -20,6 +21,15 @@ mongoose
 
 // use ports  
 app.use('/api/items', items);
+
+// server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/built'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port =  process.env.PORT || 5000;
 app.listen(port, () => console.log(`server started on port ${port}`));
