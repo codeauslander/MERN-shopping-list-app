edcd371c28006b54552f4b948cae74f765d229e7
codeauslander <cesar.gusto.catano@gmail.com>
2018-10-19 19:16:28 -0500

    Fix type error

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/api/items', items);
 // server static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // set static folder
-  app.use(express.static('client/built'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
