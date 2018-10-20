5caacfb60a557422db4329501e68a13c56e8859f
codeauslander <cesar.gusto.catano@gmail.com>
2018-10-19 19:52:06 -0500

    Added welcome message

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             <ShoppingList />
           </Container>
           
-          <h1 style={{textAlign:'center', marginTop: '2rem'}}>Welcome to the Nominator App! Where you can add items or delete them, is up to You to decide.</h1>
+          <h6 style={{textAlign:'center', marginTop: '2rem'}}>Welcome! to the Nominator App, Where you can add items or delete them, is up to you to decide.</h6>
         </div>
       </Provider> 
     );
